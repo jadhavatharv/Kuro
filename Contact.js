68b201562bde33093ad1b571219559f83e7d9ae7
Atharv Jadhav <atharvjadhav.dev@outlook.com>
2025-11-25 20:10:39 +0530

    Contact.js

--- Contact.js
+++ Contact.js
@@ -8,3 +8,7 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+
+document.getElementById("loginBtn").addEventListener("click", function() {
+        window.location.href = "Login.html"; 
+});
