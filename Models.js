1d66dd6ff87037e20228db26d63a5cdd8743f836
Atharv Jadhav <atharvjadhav.dev@outlook.com>
2025-11-28 13:02:03 +0530

    Add files via upload

---
+++ Models.js
@@ -0,0 +1,12 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+        window.location.href = "Login.html"; 
+});
+
+const menuToggle = document.getElementById('mobile-menu');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+
